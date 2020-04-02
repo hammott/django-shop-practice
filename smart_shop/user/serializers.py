fa863d439437cffd5f5e2dd1ca6c5815d389db49
Hamid Mottaghian <mottaghian.hamid@hotmail.com>
2020-04-02 01:44:45 -0700

    debugging queryset

--- smart_shop/user/serializers.py
+++ smart_shop/user/serializers.py
@@ -3,7 +3,10 @@ from rest_framework.authtoken.models import Token
 from rest_framework import serializers
 from .models import User
 
+from django.contrib.auth import password_validation
+from django.contrib.auth.models import BaseUserManager
 
+# LOGIN--------------------------------------------------------------------------
 class UserLoginSerializer(serializers.Serializer):
     email = serializers.CharField(max_length=255, required=True)
     password = serializers.CharField(required=True, write_only=True)
@@ -22,3 +25,41 @@ class AuthUserSerializer(serializers.ModelSerializer):
 
 class EmptySerializer(serializers.Serializer):
     pass
+
+
+
+# REGISTER--------------------------------------------------------------------------
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    """registering the user"""
+
+    class Meta:
+        model = User
+        fields = ('id', 'email','name', 'password')
+
+    def validate_email(self, value):
+        user = User.objects.filter(email=email)
+        if user:
+            raise serializers.ValidationError("Email is already taken")
+        return BaseUserManager.normalize_email(value)
+
+    def validate_password(self, value):
+        password_validation.validate_password(value)
+        return value
+
+
+# CHANGE PASSWORD ------------------------------------------------------------------
+
+
+class PasswordChangeSerializer(serializers.Serializer):
+    current_password = serializers.CharField(required=True)
+    new_password = serializers.CharField(required=True)
+
+    def validate_current_password(self, value):
+        if not self.context['request'].user.check_password(value):
+            raise serializers.ValidationError('Current password does not match')
+        return value
+
+    def validate_new_password(self, value):
+        password_validation.validate_password(value)
+        return value
--- smart_shop/user/utils.py
+++ smart_shop/user/utils.py
@@ -1,9 +1,24 @@
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, login
 from rest_framework import serializers
+from .models import User
 
 
-def get_and_authenticate_user(emai,password):
-    user = authenticate(username = email,  password = password)
+# LOGIN------------------------------------------------------------------------
+def get_and_authenticate_user(email,password):
+    user = authenticate(username=email, password=password)
     if user is None:
         raise serializers.ValidationError("Invalid Username or Password")
+    return user
+
+
+
+
+# REGISTER--------------------------------------------------------------------------
+def create_user_account(email, password, name="", **extra_fields):
+    user = User.objects.create_user(
+                                email=email, 
+                                password=password, 
+                                name=name,
+                                **extra_fields
+                                )
     return user
