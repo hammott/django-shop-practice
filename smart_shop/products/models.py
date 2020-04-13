436ba9b76e7d165fb3d93bc1a0368a0a8935e15b
Hamid Mottaghian <mottaghian.hamid@hotmail.com>
2020-04-13 05:59:00 -0700

    debuging

---
+++ smart_shop/products/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+class Products(models.Model):
+    title = models.CharField(max_length=255)
+    short_description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(decimal_places=2, max_digits=10)
+    
